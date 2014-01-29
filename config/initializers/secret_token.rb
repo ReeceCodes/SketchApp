0e9e5a2bbe0f26544db34b48314c23523d1fce86
Reece Hebel <reecehebel@yahoo.com>
2014-01-28 20:49:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SketchApp::Application.config.secret_key_base = 'f8648ccddfb268c5977ffaeed6fc2194c4e2280abb2e3eb5568a681633455f0c3955976223f4b3036606fd53af9595b9320a9a4c577ca93bdca9713f5cdd2d33'
